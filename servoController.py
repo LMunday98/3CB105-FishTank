fe67053de277695d342249fe4e4bb2191b0a9d72
LMunday98 <luke.munday@gmail.com>
2020-12-17 02:15:39 +0000

    tidy up on servo controller script

--- servoController.py
+++ servoController.py
@@ -5,71 +5,42 @@ import time
 class ServoController():
 
     def __init__(self):
-        # Set GPIO numbering mode
-        GPIO.cleanup()
-        GPIO.setmode(GPIO.BOARD)
-
+        self.auto = True
         self.delayTime = 3
 
-        # Set pin 11 as an output, and define as servo1 as PWM pin
+        GPIO.cleanup()
+        GPIO.setmode(GPIO.BOARD)
         GPIO.setup(11,GPIO.OUT)
-        self.servo1 = GPIO.PWM(11,50) # pin 11 for servo1, pulse 50Hz
-
-        # Start PWM running, with value of 0 (pulse off)
+        self.servo1 = GPIO.PWM(11,50)
         self.servo1.start(0)
 
-
-
     def beginServo(self):
-        #self.servoUsrInp()
-        self.servoAuto()
-
-
-
-    def servoUsrInp(self):
         try:
             while True:
-                #Ask user for angle and turn servo to it
-                angle = float(input('Enter angle between 0 & 180: '))
-                self.servo1.ChangeDutyCycle(2+(angle/18))
-                time.sleep(0.1)
-                self.servo1.ChangeDutyCycle(0)
+                if (self.auto == True):
+                    # flip
+                    self.moveServo(180)
+                    time.sleep(0.01)
+                    self.moveServo(180)
+                    time.sleep(self.delayTime)
 
+                    # reset
+                    self.moveServo(0)
+                    time.sleep(0.01)
+                    self.moveServo(0)
+                    time.sleep(self.delayTime)
+                else:
+                    angle = float(input('Enter angle between 0 & 180: '))
+                    self.moveServo(angle)
+                    time.sleep(0.1)
         finally:
-            #Clean things up at the end
             self.servo1.stop()
             GPIO.cleanup()
-            print("Goodbye!")
-
-    def servoAuto(self):
-        try:
-            while True:
-
-                self.servo1.ChangeDutyCycle(self.calcMoveAngle(180))
-                time.sleep(0.1)
-                self.servo1.ChangeDutyCycle(0)
-                time.sleep(0.1)
-
-                self.servo1.ChangeDutyCycle(self.calcMoveAngle(180))
-                time.sleep(0.1)
-                self.servo1.ChangeDutyCycle(0)
-                time.sleep(self.delayTime)
-
-                self.servo1.ChangeDutyCycle(self.calcMoveAngle(0))
-                time.sleep(0.1)
-                self.servo1.ChangeDutyCycle(0)
-                time.sleep(0.1)
-
-                self.servo1.ChangeDutyCycle(self.calcMoveAngle(0))
-                time.sleep(0.1)
-                self.servo1.ChangeDutyCycle(0)
-                time.sleep(self.delayTime)
-
-        finally:
-            #Clean things up at the end
-            self.servo1.stop()
-            GPIO.cleanup()
-            print("Goodbye!")
 
     def calcMoveAngle(self, angle):
         return (2+(angle/18))
+
+    def moveServo(self, angleToMove):
+        self.servo1.ChangeDutyCycle(self.calcMoveAngle(angleToMove))
+        time.sleep(0.1)
+        self.servo1.ChangeDutyCycle(0)
