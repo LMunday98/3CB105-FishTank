25af636360b3c25bb160a23dc135c3694b4feb71
LMunday98 <luke.munday@gmail.com>
2020-12-18 20:05:44 +0000

    refactored threading into seperate class

--- main.py
+++ main.py
@@ -2,13 +2,13 @@
 
 import sys
 import time
-import threading
 
-import datetime
+from datetime import datetime, time
 
 from tempController import TempController
 from pumpController import PumpController
 from servoController import ServoController
+from threadController import ThreadController
 
 # program setup
 
@@ -16,52 +16,18 @@ dev = False
 repeat = True
 repeat_delay = 1
 
+# setup controllers
+
 servo = ServoController(dev, repeat, repeat_delay)
 tempSensor = TempController(dev, repeat, repeat_delay)
 #pumpController = PumpController()
 
-# defs
+controller_array = []
+controller_array.append(servo)
+controller_array.append(tempSensor)
 
-def time_in_range(start, end, x):
-    """Return true if x is in the range [start, end]"""
-    if start <= end:
-        return start <= x <= end
-    else:
-        return start <= x or x <= end
-
-def create_thread(targetProcess):
-    threads.append(threading.Thread(target=start_process, args=(targetProcess,)))
-
-def start_process(process):
-    process.start()
-
-# processing
-start = datetime.time(23, 0, 0)
-end = datetime.time(1, 0, 0)
-#print(time_in_range(start, end, datetime.time()))
-
-# start multithreading
-
-try:
-    # create threads
-    print("Threading: create")
-    threads = []
-    create_thread(tempSensor)
-    create_thread(servo)
-
-    # set daemon if repeat
-    print("Threading: daemon")
-    if (repeat == True):
-        for t in threads:
-            t.setDaemon(True)
-
-    # start threads
-    print("Threading: start")
-    for t in threads:
-        t.start()
-
-except Exception as e:
-    print("Threading: error")
+thread_controller = ThreadController(controller_array, repeat)
+thread_controller.start()
 
 if (repeat == True):
     try:
---
+++ threadController.py
@@ -0,0 +1,38 @@
+import sys
+import threading
+
+class ThreadController():
+
+    def __init__(self, _controller_array, _repeat):
+        print("Thread Controller: create")
+        self.controller_array = _controller_array
+        self.repeat = _repeat
+        self.threads = []
+
+    def create_thread(self, targetProcess):
+        self.threads.append(threading.Thread(target=self.start_process, args=(targetProcess,)))
+
+    def start_process(self, process):
+        process.start()
+
+    def start(self):
+        print("Thread Controller: start")
+        try:
+            # create threads
+            print("Threading: create")
+            for controller in self.controller_array:
+                self.create_thread(controller)
+
+            # set daemon if repeat
+            print("Threading: daemon")
+            if (self.repeat == True):
+                for t in self.threads:
+                    t.setDaemon(True)
+
+            # start threads
+            print("Threading: start")
+            for t in self.threads:
+                t.start()
+
+        except Exception as e:
+            print("Threading: error")
